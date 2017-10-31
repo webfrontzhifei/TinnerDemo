031576908861e15edeaad349ed221fa6859aa873
webfrontzhifei <906831852@qq.com>
2017-11-01 01:24:30 +0800

    simple demo

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,8 @@ var jshint = require('gulp-jshint');
 var concat = require('gulp-concat');
 var stripDebug = require('gulp-strip-debug');
 var uglify = require('gulp-uglify');
+var autoprefix = require('gulp-autoprefixer');
+var minifyCSS = require('gulp-minify-css');
 
 gulp.task('jshint', function() {
     gulp.src('./src/scripts/*.js')
@@ -17,4 +19,21 @@ gulp.task('scripts', function() {
         .pipe(stripDebug())
         .pipe(uglify())
         .pipe(gulp.dest('./build/scripts/'));
+});
+
+gulp.task('styles', function() {
+    gulp.src(['./src/styles/*.css'])
+        .pipe(concat('styles.css'))
+        .pipe(autoprefix('last 2 versions'))
+        .pipe(minifyCSS())
+        .pipe(gulp.dest('./build/styles/'));
+})
+
+gulp.task('default', ['scripts', 'styles'], function() {
+    gulp.watch('./src/scripts/*.js', function() {
+        gulp.run('jshint','scripts');
+    });
+    gulp.watch('./src/styles/*.css', function() {
+        gulp.run('styles');
+    });
 })
